cf0a4acf71a05d50938182d788b4690c7adbcf80
Dan Cecile <dancecile@gmail.com>
2018-05-19 08:41:48 -0400

    Reset base colors to simple black and white 🎨
    Start over with higher contrast. Alternatives to be checked again once all UI
    elements added to page.
    
    For #9

--- jsShell/Colors.scala
+++ jsShell/Colors.scala
@@ -7,13 +7,13 @@ import CssSettings.Defaults._
 object Colors extends StyleSheet.Inline {
   import Colors.dsl._
 
-  val background: Color = c"#1a1a1a"
+  val background: Color = c"#000"
   val edgeHighlight: Color = c"rgba(229, 255, 229, 0.08)"
   val error: Color = c"#e82d2d"
   val highlight: Color = c"#d3a7ff"
   val highlightGlow: Color = c"rgba(229, 255, 229, 0.41)"
   val link: Color = c"#9069b3"
-  val normal: Color = c"#e5ffe5"
+  val normal: Color = c"#fff"
   val selection: Color = c"rgba(167, 140, 151, 0.99)"
   val value: Color = c"#46f6f6"
 }
