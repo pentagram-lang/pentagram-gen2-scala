0f1fd10e2e4333c75a646908acf1004096a28392
Dan Cecile <dancecile@gmail.com>
2018-06-22 22:23:17 -0400

    Again (PR3)

--- jsShell/HomeTitle.scala
+++ jsShell/HomeTitle.scala
@@ -11,7 +11,7 @@ object HomeTitle extends RenderSimple[html.Heading] {
   def renderSimple: RenderTag =
     h1(
       Style.root,
-      "Welcome to Tacit! (PR2)"
+      "Welcome to Tacit! (PR3)"
     )
 
   object Style extends StyleSheet.Inline {
